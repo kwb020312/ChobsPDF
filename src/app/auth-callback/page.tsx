75f07888e543266939e8de7f14a5f002359cf6a0
김우빈 <kwb020312@naver.com>
2023-10-22 18:57:35 +0900

    Feat: tRPC Setup

--- src/app/auth-callback/page.tsx
+++ src/app/auth-callback/page.tsx
@@ -1,5 +1,6 @@
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
+import { trpc } from "../_trpc/client";
 
 const Page = async () => {
   const router = useRouter();
@@ -7,9 +8,14 @@ const Page = async () => {
   const searchParams = useSearchParams();
   const origin = searchParams.get("origin");
 
-  const apiResponse = await fetch("/api/whatever");
-
-  const data = await apiResponse.json();
+  const { data, isLoading } = trpc.authCallback.useQuery(undefined, {
+    onSuccess: ({ success }) => {
+      if (success) {
+        // user is synced to db
+        router.push(origin ? `/${origin}` : "/dashboard");
+      }
+    },
+  });
 };
 
 export default Page;
---
+++ src/trpc/index.ts
@@ -0,0 +1,17 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { router, publicProcedure } from "./trpc";
+import { TRPCError } from "@trpc/server";
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(() => {
+    const { getUser } = getKindeServerSession();
+    const user = getUser();
+
+    if (!user.id || !user.email) throw new TRPCError({ code: "UNAUTHORIZED" });
+
+    // check if the user is in the database
+    return { success: true };
+  }),
+});
+
+export type AppRouter = typeof appRouter;
