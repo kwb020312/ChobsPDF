f3e2d376324bf0e7d31c0018e573c3dc231b073f
김우빈 <kwb020312@naver.com>
2023-10-23 20:58:27 +0900

    Feat: File Detail

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,32 @@
+import { db } from "@/db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+
+interface PageProps {
+  params: {
+    fileid: string;
+  };
+}
+
+const page = async ({ params }: PageProps) => {
+  // retrieve the file id
+  const { fileid } = params;
+  // make database call
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  if (!user || !user.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+
+  if (!file) notFound();
+
+  return <div className="">{fileid}</div>;
+};
+
+export default page;
