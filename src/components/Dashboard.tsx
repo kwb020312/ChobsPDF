8ba3c906cc62c46933add3b0c3ed55d9ee9253ac
김우빈 <kwb020312@naver.com>
2023-10-23 20:28:00 +0900

    Feat: Delete PDF Project

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,14 +2,33 @@
 
 import { trpc } from "@/app/_trpc/client";
 import UploadButton from "./UploadButton";
-import { Ghost, MessageSquare, Plus, Trash } from "lucide-react";
+import { Ghost, Loader2, MessageSquare, Plus, Trash } from "lucide-react";
 import Skeleton from "react-loading-skeleton";
 import Link from "next/link";
 import { format } from "date-fns";
 import { Button } from "./ui/button";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [currentlyDeletingFile, setCurrentlyDeletingFile] = useState<
+    string | null
+  >(null);
+
+  const utils = trpc.useContext();
+
   const { data: files, isLoading } = trpc.getUserFiles.useQuery();
+
+  const { mutate: deleteFile } = trpc.deleteFile.useMutation({
+    onSuccess: () => {
+      utils.getUserFiles.invalidate();
+    },
+    onMutate({ id }) {
+      setCurrentlyDeletingFile(id);
+    },
+    onSettled() {
+      setCurrentlyDeletingFile(null);
+    },
+  });
   return (
     <main className="mx-auto max-w-7xl md:p-10">
       <div className="mt-8 flex flex-col items-start justify-between gap-4 border-b border-gray-200 pb-5 sm:flex-row sm:items-center sm:gap-0">
@@ -60,11 +79,16 @@ const Dashboard = () => {
                   </div>
 
                   <Button
+                    onClick={() => deleteFile({ id: file.id })}
                     size={"sm"}
                     className="w-full"
                     variant={"destructive"}
                   >
-                    <Trash className="h-4 w-4" />
+                    {currentlyDeletingFile === file.id ? (
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    ) : (
+                      <Trash className="h-4 w-4" />
+                    )}
                   </Button>
                 </div>
               </li>
--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -40,8 +40,25 @@ export const appRouter = router({
   }),
   deleteFile: privateProcedure
     .input(z.object({ id: z.string() }))
-    .mutation(({ ctx }) => {
+    .mutation(async ({ ctx, input }) => {
       const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          id: input.id,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      await db.file.delete({
+        where: {
+          id: input.id,
+        },
+      });
+
+      return file;
     }),
 });
 
