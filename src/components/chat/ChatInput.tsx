2e92b3b92d8bf790bca282ebbe413f593fb495a9
김우빈 <kwb020312@naver.com>
2023-10-27 15:20:09 +0900

    Feat: Message Section Layout

---
+++ src/components/chat/ChatInput.tsx
@@ -0,0 +1,9 @@
+interface ChatInputProps {
+  isDisabled?: boolean;
+}
+
+const ChatInput = ({ isDisabled }: ChatInputProps) => {
+  return <div>ChatInput</div>;
+};
+
+export default ChatInput;
---
+++ src/components/chat/Messages.tsx
@@ -0,0 +1,5 @@
+const Messages = () => {
+  return <div className=""></div>;
+};
+
+export default Messages;
